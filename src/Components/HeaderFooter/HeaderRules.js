05395252553ac91f180e1b7f835a3b6566cc1c19
verrakav <verrakav@users.noreply.github.com>
2024-01-29 19:56:45 +1100

    popup + readme

--- src/Components/HeaderFooter/HeaderRules.js
+++ src/Components/HeaderFooter/HeaderRules.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./HeaderRules.css";
+import PopupWindow from "./PopupWindow";
+// import Popup from "reactjs-popup";
+// import CIcon from "@coreui/icons-react";
+// import * as icon from "@coreui/icons";
 
 //TODO: add the popup window with the rules
 //at the bot of the page (don't forget the styling)
 function HeaderRules() {
   return (
     <header className="header-rules">
-      <a href="#" className="popup-rules">
-        How to play?
-      </a>
+      <PopupWindow />
       <span className="centre">Let's Play Goroda!</span>
     </header>
   );
---
+++ src/Components/HeaderFooter/PopupWindow.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import Popup from "reactjs-popup";
+//library docs: https://react-popup.elazizi.com/controlled-popup/
+import CIcon from "@coreui/icons-react";
+import * as icon from "@coreui/icons";
+
+function PopupWindow() {
+  //NOTE: below is the working variant
+  const [open, setOpen] = useState(false);
+  const closePop = () => {
+    setOpen(false);
+  };
+  return (
+    //library component with some built-in stuff, see config: https://minutemailer.github.io/react-popup/
+    <Popup
+      open={open}
+      onClick={closePop}
+      trigger={
+        <a href="#" className="popup-trigger">
+          How to play?
+        </a>
+      }
+      //should dim the background
+      modal
+    >
+      {/*NOTE: this is function as a cild (render prop) 0_o help */}
+      {/* anonymous function passed as a prop with the argument that is an action(funuctionality)
+here close comes from the library, right? */}
+      {(close) => (
+        <p className="popup-window">
+          <CIcon
+            icon={icon.cilX}
+            className="icon"
+            onClick={() => {
+              close();
+            }}
+          />
+          The game is based on the first and the last letters of citites. <br />
+          If the first player says: Melbourne, the second player should think of
+          a city starting with an 'E' - Edinburgh - then the next should start
+          with an 'H'!
+        </p>
+      )}
+    </Popup>
+  );
+}
+
+export default PopupWindow;
