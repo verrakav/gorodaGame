4e7a0ad76645402306cc5ea0489be2e49e961678
verrakav <verrakav@users.noreply.github.com>
2024-03-31 20:43:07 +1100

    map+marker workkk

--- src/Components/Mapshow/MapShow.js
+++ src/Components/Mapshow/MapShow.js
@@ -1,36 +1,21 @@
+import React from "react";
 import { Map, Marker } from "pigeon-maps";
-import { useState, useEffect, useRef } from "react";
-// import { fetchCoordinates } from "../../utils/APIrelated";
-// import { manageRandomLocation } from "../../utils/Managers";
 
-//FIXME: city is not valid var
-function MapShow({ computerResponseCity, inputCity, userCityMessage }) {
-  const [center, setCenter] = useState([]);
-  const [zoom, setZoom] = useState();
-
-  const mapRef = useRef(null);
-
-  useEffect(() => {
-    if (mapRef.current && (inputCity || computerResponseCity)) {
-      //NOTE: this gets lat & long
-      // fetchCoordinates();
-      //   mapRef.current.flyTo([city.latitude, city.longitude]);
-    }
-  }, [computerResponseCity, userCityMessage]);
-
-  // const defaultCentre = manageRandomLocation();
-
+//need to receive coordinates
+function MapShow({ center, setCenter, zoom, setZoom }) {
+  //use coordinates here
+  // const [center, setCenter] = useState([50.1109, 8.6821]);
+  // const [zoom, setZoom] = useState(11);
   return (
-    //change default centre
     <Map
-      height={400}
-      defaultCenter={[50.879, 4.6997]}
-      defaultZoom={11}
+      height={350}
+      center={center}
+      zoom={zoom}
       onBoundsChanged={({ center, zoom }) => {
         setCenter(center);
         setZoom(zoom);
       }}>
-      <Marker width={50} position={[50.879, 4.6997]} />
+      <Marker width={50} anchor={center} />
     </Map>
   );
 }
