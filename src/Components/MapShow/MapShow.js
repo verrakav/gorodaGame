3a514ad14b1650b10bc0d2f5c38a1076fc03e69e
verrakav <verrakav@users.noreply.github.com>
2024-04-01 17:13:44 +1100

    raw working version
    TODO:
    - fix the scoring system;
    - fix the marker;
    - look into corner cases (Ufa);
    - heaps to refactor

--- src/Components/MapShow/MapShow.js
+++ src/Components/MapShow/MapShow.js
@@ -3,9 +3,6 @@ import { Map, Marker } from "pigeon-maps";
 
 //need to receive coordinates
 function MapShow({ center, setCenter, zoom, setZoom }) {
-  //use coordinates here
-  // const [center, setCenter] = useState([50.1109, 8.6821]);
-  // const [zoom, setZoom] = useState(11);
   return (
     <Map
       height={350}
